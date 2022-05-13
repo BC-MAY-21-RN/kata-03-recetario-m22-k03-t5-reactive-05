97360d2b49f8204286950fc3c1ebfe7f229f2a67
Jose Estrada <j.estrada.developer@gmail.com>
2022-05-13 18:09:34 -0500

    Add Navigation component
    Creation of Navigation component with Home and Recipe Screens in the stack navigator
    
    Co-authored-by: ShamuXX <samuel16mendoza@gmail.com>
    Co-authored-by: jestradadeveloper <j.estrada.developer@gmail.com>
    Co-authored-by: LizyMiGo <jmiguel@ucol.mx>

---
+++ recetarioCocina/src/components/navigation/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {HomeScreen, RecipeScreen} from '../../screens';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Recipe" component={RecipeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
